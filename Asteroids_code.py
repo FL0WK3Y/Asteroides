0774a8f23c72afd0a964b81cff818635111322b4
gregori <g.rodriguez24680@gmail.com>
2023-12-28 14:18:43 -0500

    split the asteroids when hit

--- Asteroids_code.py
+++ Asteroids_code.py
@@ -3,6 +3,11 @@ from pygame import Vector2
 import random
 from pygame.transform import rotozoom
 import os
+from pygame.mixer import Sound
+
+asteroid_images = ['/Users/gregorirodriguez/Desktop/Games/Asteroids/images/asteroid1.png','/Users/gregorirodriguez/Desktop/Games/Asteroids/images/asteroid2.png', '/Users/gregorirodriguez/Desktop/Games/Asteroids/images/asteroid3.png']
+
+
 # this class is so that when the ship goes off screen it raps around to the other side 
 def wrap_position(position, screen):
     x, y = position 
@@ -26,6 +31,7 @@ class Ship:
         self.bullets = []
         self.can_shoot = 0
         self.drift = (0,0)
+        self.shoot = Sound(os.path.join('/Users/gregorirodriguez/Desktop/Games/Asteroids/asteroidSounds/shoot.wav'))
 
 
     
@@ -44,6 +50,9 @@ class Ship:
         if is_key_pressed [pygame.K_SPACE] and self.can_shoot == 0 :
             # this code is for when the bullet is created "fired" and we need to pass the position of the ship and the direction the bullet is going 
             self.bullets.append(Bullet(Vector2(self.position),self.forward * 10))
+            #this play the shooting sound when you press the space bar
+            self.shoot.play()
+
             # this is the amount of second you need to wait before a new bullet can be fired 
             self.can_shoot= 400
         self.position+= self.drift
@@ -75,11 +84,14 @@ class Bullet:
 
 # this class creates the Asteroid
 class Asteroid:
-    def __init__(self, position):
+    def __init__(self, position, size):
         self.position = Vector2(position)
         self.velocity = Vector2(random.randint(-3,3),random.randint(-3,3))
-        self.image= pygame.image.load('/Users/gregorirodriguez/Desktop/Games/Asteroids/images/asteroid1.png')
+        self.image= pygame.image.load(asteroid_images[size])
         self.radius = self.image.get_width()//2
+        self.explode = Sound(os.path.join('/Users/gregorirodriguez/Desktop/Games/Asteroids/asteroidSounds/explode.mp3'))
+        self.size = size
+        
     #this updates the velocity of the asteriod
     def update(self):
         self.position+= self.velocity
@@ -99,8 +111,13 @@ class Asteroid:
     #this is collision detection 
     def hit(self, position):
         if self.position.distance_to(position)<= self.radius:
+            #this plays the sound when any object get "hit" 
+            self.explode.play()
             return True
         return False
+
+   
+    
 
 pygame.init()
 screen = pygame.display.set_mode((800,800))
@@ -111,15 +128,27 @@ background = pygame.image.load(os.path.join('/Users/gregorirodriguez/Desktop/Gam
 
 
 game_over = False
-ship = Ship((100,700,))
+ship = Ship((screen.get_width()//2 ,screen.get_height()//2))
 asteroids = []
 #this is the max radios that the asteroids can move until they return back 
 out_of_bounce = [-150,-150, 950, 950]
 
 # this code make 10 astoriods in the screen 
-for i in range(10):
-        asteroids.append(Asteroid((random.randint(0,screen.get_width()),random.randint(0,screen.get_height()))))
+
+
+for i in range(5):
+        asteroids.append(Asteroid((random.randint(0,screen.get_width()),random.randint(0,screen.get_height())),0))
 clock = pygame.time.Clock()
+
+font = pygame.font.SysFont('Arial',80, True, False)
+text_loser = font.render("  YOU LOST ! " , True, (255,255,255))
+text_loser_position= ((screen.get_width()-text_loser.get_width())//2,(screen.get_height()-text_loser.get_height())//2)
+
+
+text_win = font.render("  YOU WIN ! " , True, (255,255,255))
+text_win_position= ((screen.get_width()-text_win.get_width())//2,(screen.get_height()-text_win.get_height())//2)
+
+
 
 # this is the while loop that runs throught the game 
 while not game_over:
@@ -128,6 +157,16 @@ while not game_over:
         if event.type == pygame.QUIT:
             game_over = True
     screen.blit(background, (0,0))
+
+    if ship is None:
+        screen.blit(text_loser, text_loser_position)
+        pygame.display.update()
+        continue
+
+    if len(asteroids)==0:
+        screen.blit(text_win, text_win_position)
+        pygame.display.update()
+        continue
     # this updates the ship on the game window
     ship.update()
     ship.draw(screen)
@@ -135,6 +174,12 @@ while not game_over:
     for a in asteroids:
         a.update()
         a.draw(screen)
+
+        if a.hit(ship.position):
+            ship = None
+            break
+    if ship is None:
+        continue
     
     deadBullets = []
     deadAsteroids = []
@@ -154,11 +199,15 @@ while not game_over:
                     deadAsteroids.append(a)
 
     for b in deadBullets:
+        
+        
         ship.bullets.remove(b)
 
     for a in deadAsteroids:
-        asteroids.remove(a)
-        
+        if a.size <2: 
+            asteroids.append(Asteroid(a.position, a.size +1))
+            asteroids.append(Asteroid(a.position, a.size +1))
+        asteroids.remove(a)    
 
 
     pygame.display.update()
